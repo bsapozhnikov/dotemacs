63016be0fe00806c73d70c9681a46c08a4f522af
Brian Sapozhnikov <brian.sapozhnikov@gmail.com>
2022-01-09 11:00:03 -0500

    updated .emacs.d

--- .emacs.d/elpa/jsx-mode-20130908.1024/jsx-mode-autoloads.el
+++
@@ -1,29 +0,0 @@
-;;; jsx-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (jsx-mode) "jsx-mode" "jsx-mode.el" (22090 19831
-;;;;;;  0 0))
-;;; Generated autoloads from jsx-mode.el
-
-(autoload 'jsx-mode "jsx-mode" "\
-
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("jsx-mode-pkg.el") (22090 19831 787000
-;;;;;;  0))
-
-;;;***
-
-(provide 'jsx-mode-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; jsx-mode-autoloads.el ends here
